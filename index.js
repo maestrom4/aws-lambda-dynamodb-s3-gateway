20dcfb86a8ad848d5549048e97a7c505f1858d48
maestrom4 <maestro.m4@gmail.com>
2022-02-14 12:23:02 +0800

    update index

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ AWS.config.update({
 });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const dynamodbTableName = 'product-inventory';
+// const dynamodbTableName = 'product-inventory';
+const loginTbl = 'logInTbl';
+const homePath = '/';
 const loginPath = '/login';
 const schedulePath = '/schedule';
 const usersPath = '/users';
@@ -13,8 +15,8 @@ exports.handler = async function(event) {
     console.log('Request event: ', event);
     let response;
     switch(true) {
-        case event.httpMethod === 'GET' && event.path === loginPath:
-            response = await loginPath(200);
+        case event.httpMethod === 'GET' && event.path === homePath:
+            response = await buildResponse(200);
             break;
         case event.httpMethod === 'GET' && event.path === schedulePath:
             response = await getSchedule(event.queryStringParameters.scheduleId);
@@ -25,13 +27,54 @@ exports.handler = async function(event) {
         case event.httpMethod === 'POST' && event.path === loginPath:
             response = await login(JSON.parse(event.body));
             break;
-        case event.httpMethod === 'POST' && event.path === loginPath:
-            response = await login(JSON.parse(event.body));
+        case event.httpMethod === 'PATCH' && event.path === loginPath:
+            const req = JSON.parse(event.body);
+            response = await updateLoginData(req.id, req.ukey, req.val);
             break;
+        case event.httpMethod === 'DELETE' && event.path === loginPath:
+            response = await updateLoginData(JSON.parse(event.body).id);
+            break;
+        default:
+            response = buildResponse(404, '404 not found!');
     }
 }
 
+
+function updateLoginData(reqID, reqUkey, reqVal) {
+    const params = {
+        TableName: loginTbl,
+        Key: {
+            'loginKey': reqID
+        },
+        UpdateExpression: `set ${reqUkey} = :value`,
+        ExpressionAttributeValues: {
+            ':value': reqVal
+        },
+        ReturnValues: 'UPDATE_NEW'
+    }
+    return await dynamodb.update(params).promise().then((response) => {
+        const body = {
+            Opearion: 'UPDATE',
+            Message: 'SUCCESS',
+            UpdateAttributes: response
+        }
+        return buildResponse(200, body)
+    }, (error) => {
+        console.error('Do your custom error handling here. I am just gonna log it: ', error)
+    })
+}
+
 function loginPath(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+};
+
+function buildResponse(statusCode, body = {}) {
     return {
         statusCode: statusCode,
         headers: {
