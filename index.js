148544402bd18126a24b12f54f7bdd9219b78bfd
maestrom4 <maestro.m4@gmail.com>
2022-02-14 08:12:50 +0800

    update index

--- index.js
+++ index.js
@@ -5,5 +5,35 @@ AWS.config.update({
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamodbTableName = 'product-inventory';
-const healthPath = '/health';
-const productPath = '/product';
+const loginPath = '/login';
+const schedulePath = '/schedule';
+const usersPath = '/users';
+
+exports.handler = async function(event) {
+    console.log('Request event: ', event);
+    let response;
+    switch(true) {
+        case event.httpMethod === 'GET' && event.path === loginPath:
+            response = await loginPath(200);
+            break;
+        case event.httpMethod === 'GET' && event.path === schedulePath:
+            response = await getSchedule(event.queryStringParameters.scheduleId);
+            break;
+        case event.httpMethod === 'GET' && event.path === usersPath:
+            response = await getUsers(event.queryStringParameters.userId);
+            break;
+        case event.httpMethod === 'POST' && event.path === loginPath:
+            response = await login(JSON.parse(event.body));
+            break;
+    }
+}
+
+function loginPath(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+};
